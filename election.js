df5f99c07499554c075688fd0109009cdb072d41
tylteng <teng.ty4@gmail.com>
2018-01-04 18:09:36 -0500

    completed part 1 of election

--- election.js
+++ election.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+var candidatesList = document.querySelector('#candidates-list')
+
   // Imagination!
+
+$.ajax({
+  url: 'https://bb-election-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'JSON'
+}).done(function(responseData){
+  console.log(responseData['candidates'][0]);
+  for(var index = 0; index < responseData['candidates'].length; index++){
+    var candidate = document.createElement('li');
+    var candidateData = ("Name: " + responseData['candidates'][index].name + " " + "Votes: " + responseData['candidates'][index].votes);
+    candidate.innerText = candidateData
+    candidatesList.appendChild(candidate)
+  };
+});
 
 });
